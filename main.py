78427e64476eb936d518494d0b8210392dd1dc16
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2022-02-17 21:29:11 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+import subprocess as sp
+from time import sleep
+import json
+from backports.zoneinfo import ZoneInfo
+import dateutil.parser
+
+
+def strike(**kwargs):
+    return sp.Popen(cmd,shell=True,**kwargs)
+
+
+delta = timedelta(seconds=1)
+tz=ZoneInfo("Europe/Warsaw")
+
+# from Chrome DevTools
+cmd = "curl ... | jq"
+
+if __name__ == "__main__":
+
+    with strike(stdout=sp.PIPE) as child:
+        output = child.stdout.read()
+        obj = json.loads(output)
+        localDate = datetime.now(tz=tz)
+        remoteDateStr = obj["params"]["now"]
+        remoteDate = dateutil.parser.parse(remoteDateStr).replace(tzinfo=tz)
+        remoteGoalStr = obj["params"]["openDate"]
+        remoteGoal = dateutil.parser.parse(remoteGoalStr).replace(tzinfo=tz)
+        offset = remoteDate-localDate
+
+    def getRemoteDate():
+        return datetime.now(tz=tz)+offset
+
+    while True:
+        realDelta = remoteGoal - getRemoteDate()
+        print(f"Strike in: {realDelta}")
+        if realDelta > delta:
+            sleep(delta.total_seconds())
+        else:
+            sleep(realDelta.total_seconds())
+            break
+
+    strike()
+    sleep(0.1)
+    strike()
+    sleep(0.1)
+    strike()
+    sleep(0.2)
+    strike()
+    sleep(0.3)
+    strike()
+    sleep(1)
+    strike()
